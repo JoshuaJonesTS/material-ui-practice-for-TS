17a392003f6708c489b675c79b7a7b71bebe571f
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-18 13:09:30 -0800

    Import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import MainButton from "./blocks/Button/MainButton";
 import MainTextField from "./blocks/TextField/MainTextField";
+import MainForm from "./collections/Form/MainForm";
 
 function App() {
   return (
     <div className="App">
       <MainTextField></MainTextField>
       <MainButton></MainButton>
+      <MainForm></MainForm>
     </div>
   );
 }
