de0ac7620458abc119fb7015cd42b292e961f312
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-18 11:05:44 -0800

    Create basic text field

---
+++ src/blocks/TextField/MainTextField.js
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+
+function MainTextField () {
+    return (
+        <div>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+    );
+}
+
+export default MainTextField;
