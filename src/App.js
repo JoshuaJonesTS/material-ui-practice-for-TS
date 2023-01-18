62f6c6c3fd2567b5688a75f8d36e884d64530b2f
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-18 11:07:51 -0800

    Import text field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import MainButton from "./blocks/Button/MainButton";
+import MainTextField from "./blocks/TextField/MainTextField";
 
 function App() {
   return (
     <div className="App">
+      <MainTextField></MainTextField>
       <MainButton></MainButton>
     </div>
   );
