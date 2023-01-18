a17cc1bddd81ded4b8f8704757abeb04f948551e
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-18 13:47:50 -0800

    Spacing

--- src/blocks/Search/MainSearch.js
+++ src/blocks/Search/MainSearch.js
@@ -1,15 +1,15 @@
 function MainSearch() {
     // 1) Loop through the list of objects and do the following on each object:
 
-        // 1.1) Retrieve the value inside of the object that corresponds with the Search
-        // Field specified by the user. (For example, if the search field is “age”, then
-        // get the “age” value inside of the object)
+    // 1.1) Retrieve the value inside of the object that corresponds with the Search
+    // Field specified by the user. (For example, if the search field is “age”, then
+    // get the “age” value inside of the object)
 
-        // 1.2) Check if the search value from the user matches the value inside of the object.
+    // 1.2) Check if the search value from the user matches the value inside of the object.
 
-            // 1.2.1) If it matches, add the object to the search results list
+    // 1.2.1) If it matches, add the object to the search results list
 
-            // 1.2.2) If it does not match, do nothing.
+    // 1.2.2) If it does not match, do nothing.
 
     // 2) Return the list of search results
 
