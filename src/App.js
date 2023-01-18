9ff9da53224010ad2a86924718d654d7b0a61c24
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-18 13:36:13 -0800

    Import and render MainSearch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import MainButton from "./blocks/Button/MainButton";
 // import MainTextField from "./blocks/TextField/MainTextField";
 // import MainForm from "./collections/Form/MainForm";
+import MainSearch from "./blocks/Search/MainSearch";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* <MainTextField></MainTextField>
       <MainButton></MainButton>
       <MainForm></MainForm> */}
+      <MainSearch></MainSearch>
     </div>
   );
 }
