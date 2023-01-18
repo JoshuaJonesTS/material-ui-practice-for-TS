81a9de3db99ee87ec2232ec09b3e1459d8c39049
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-18 14:14:04 -0800

    Put pseudocode inside of return
    And create input element

--- src/blocks/Search/MainSearch.js
+++ src/blocks/Search/MainSearch.js
@@ -27,21 +27,20 @@ function MainSearch() {
         console.log(name);
     });
 
-    // 1.1) Retrieve the value inside of the object that corresponds with the Search
-    // Field specified by the user. (For example, if the search field is “age”, then
-    // get the “age” value inside of the object)
-
-    // 1.2) Check if the search value from the user matches the value inside of the object.
-
-    // 1.2.1) If it matches, add the object to the search results list
-
-    // 1.2.2) If it does not match, do nothing.
-
-    // 2) Return the list of search results
-
     return (
         <div>
+            <input type="text"></input>
+            {/* 1.1) Retrieve the value inside of the object that corresponds with the Search
+            Field specified by the user. (For example, if the search field is “age”, then
+            get the “age” value inside of the object) */}
 
+            {/* 1.2) Check if the search value from the user matches the value inside of the object. */}
+
+            {/* 1.2.1) If it matches, add the object to the search results list */}
+
+            {/* 1.2.2) If it does not match, do nothing. */}
+
+            {/* 2) Return the list of search results */}
         </div>
     );
 }
