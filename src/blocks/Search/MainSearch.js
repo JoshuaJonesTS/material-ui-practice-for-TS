ffcf3342a7643831b62daa0b356aa0e66bcb75ec
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-18 13:46:54 -0800

    Edit

--- src/blocks/Search/MainSearch.js
+++ src/blocks/Search/MainSearch.js
@@ -1,17 +1,17 @@
 function MainSearch() {
-    // 1. Loop through the list of objects and do the following on each object:
+    // 1) Loop through the list of objects and do the following on each object:
 
-        // 1. Retrieve the value inside of the object that corresponds with the Search
+        // 1.1) Retrieve the value inside of the object that corresponds with the Search
         // Field specified by the user. (For example, if the search field is “age”, then
         // get the “age” value inside of the object)
 
-        // 2. Check if the search value from the user matches the value inside of the object.
+        // 1.2) Check if the search value from the user matches the value inside of the object.
 
-            // 1. If it matches, add the object to the search results list
+            // 1.2.1) If it matches, add the object to the search results list
 
-            // 2. If it does not match, do nothing.
+            // 1.2.2) If it does not match, do nothing.
 
-    // 2. Return the list of search results
+    // 2) Return the list of search results
 
     return (
         <div>
